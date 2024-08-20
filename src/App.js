f97d9cc49eb3b3a3d57c0db960caf3c1be746588
Sergey Bulaev <s@bulaev.org>
2024-08-20 09:11:13 +0000

    several clicky fixes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React, { Suspense, lazy } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React, { Suspense, lazy, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch, useLocation } from 'react-router-dom';
 import ClickyTracker from './clickytracker';
-import Home from './Home';
+
+// Add this line to import the Home component
+const Home = lazy(() => import('./Home'));
+
+const PageViewTracker = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (window.clicky) {
+      window.clicky.log(location.pathname + location.search, document.title);
+    }
+  }, [location]);
+
+  return null;
+};
 
 const App = () => {
   return (
     <Router>
       <ClickyTracker />
+      <PageViewTracker />
       <Suspense fallback={<div>Загрузка...</div>}>
         <Switch>
           <Route exact path="/" component={Home} />
--- src/clickytracker.js
+++ src/clickytracker.js
@@ -2,14 +2,30 @@ import React, { useEffect } from 'react';
 
 const ClickyTracker = () => {
   useEffect(() => {
-    const script = document.createElement('script');
-    script.async = true;
-    script.setAttribute('data-id', '101462363');
-    script.src = '//static.getclicky.com/js';
-    document.body.appendChild(script);
+    if (!window.clicky_site_ids) {
+      window.clicky_site_ids = [101462363];
+      const script = document.createElement('script');
+      script.async = true;
+      script.setAttribute('data-id', '101462363');
+      script.src = '//static.getclicky.com/js';
+      document.head.appendChild(script);
+    }
+
+    // Manually trigger a pageview on route change
+    const triggerPageview = () => {
+      if (window.clicky) {
+        window.clicky.log(window.location.pathname + window.location.search, document.title);
+      }
+    };
+
+    // Initial pageview
+    triggerPageview();
+
+    // Listen for route changes
+    window.addEventListener('popstate', triggerPageview);
 
     return () => {
-      document.body.removeChild(script);
+      window.removeEventListener('popstate', triggerPageview);
     };
   }, []);
 
